0ddb30414760a63cccf7a2b66494ceb9998517d1
Richard <zfanli@outlook.com>
2019-09-15 20:37:55 +0800

    Initial commit for Struts2 demo of training use

---
+++ src/main/java/demo4training/actions/RateAction.java
@@ -0,0 +1,30 @@
+package demo4training.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+import demo4training.model.Rate;
+import org.json.JSONObject;
+
+public class RateAction extends ActionSupport {
+
+    private String result;
+
+    private double prev = 1.0;
+
+
+    public String execute() {
+        result = new JSONObject(new Rate(prev)).toString();
+        return "json";
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public double getPrev() {
+        return prev;
+    }
+
+    public void setPrev(double prev) {
+        this.prev = prev;
+    }
+}
---
+++ src/main/java/demo4training/model/Rate.java
@@ -0,0 +1,18 @@
+package demo4training.model;
+
+public class Rate {
+    public double getRate() {
+        return rate;
+    }
+
+    public Rate(double rate) {
+        this.rate = rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    private double rate;
+
+}
